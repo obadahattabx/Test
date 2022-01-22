f611a6dc0aa971861153862338da30d80ec84921
hp <yazandoha@hotmail.com>
2022-01-23 00:43:40 +0200

    yazan objecting

--- app/src/main/java/com/hattab/myapplicationtest/MainActivity2.java
+++ app/src/main/java/com/hattab/myapplicationtest/MainActivity2.java
@@ -3,6 +3,7 @@ package com.hattab.myapplicationtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button mm;
+
     }
 }
